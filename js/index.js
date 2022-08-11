e312ee616aadcb56bd09e0b99054d462467b3a61
Manuel Alfaro <manuel.alfaro1488@gmail.com>
2022-08-10 20:59:30 -0500

    basic info

--- js/index.js
+++ js/index.js
@@ -5,19 +5,23 @@ $(document).ready(function () {
     });
 
     $('#base_experience').jqxInput({        
-        width: 100        
+        width: 100,
+        disabled: true
     });
 
     $('#height').jqxInput({        
-        width: 100        
+        width: 100,
+        disabled: true    
     });
     
     $('#name').jqxInput({        
-        width: 100        
+        width: 100,
+        disabled: true
     });
 
     $('#id').jqxInput({        
-        width: 100        
+        width: 100,
+        disabled: true
     });
   
 
@@ -32,10 +36,13 @@ function ViewModel(){
     self.PokemonName = ko.observable('');
     self.SearchMessage = ko.observable('');
     self.ShowMessage = ko.observable(false);
+    self.BaseExperience = ko.observable(0);
+    self.Height = ko.observable(0);
+    self.Id = ko.observable(0);
+    self.Name = ko.observable('');
 
-    self.PokemonName.subscribe(async function(value){
-        debugger
-        console.log(`Name: ${value}`);
+    self.PokemonName.subscribe(async function(value){        
+        console.log(`Pokemon Name: ${value}`);
 
         const url = `https://pokeapi.co/api/v2/pokemon/${value}`;
         let response = await fetch(url);
@@ -45,11 +52,19 @@ function ViewModel(){
             self.ShowMessage(false);
 
             let result = await response.json();
+            self.BaseExperience(result.base_experience);
+            self.Height(result.height);
+            self.Id(result.id);
+            self.Name(result.name);
             console.log(result);
         }
         else{
             self.SearchMessage('Pokemon no found');
             self.ShowMessage(true);
+            self.BaseExperience(0);
+            self.Height(0);
+            self.Id(0);
+            self.Name('');
         }        
     });
 }
